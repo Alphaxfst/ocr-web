38592a15f3c02b26982136c566291a1f97eaa1bf
Alpha Fausta <672019219@student.uksw.edu>
2022-11-08 14:10:00 +0700

    Buat collection user

--- db.py
+++ db.py
@@ -5,23 +5,40 @@ class Mongo():
         self.__connection = "mongodb://localhost:27017/"
         self.__creds = MongoClient(self.__connection)
         self.__document = self.__creds.db_ocr
-        self.__collection = self.__document.ocr
+        self.__ocrCollection = self.__document.ocr
+        self.__usersCollection = self.__document.ocr
     
     def loadDocument(self):
         return self.__document
     
-    def loadCollection(self):
-        return self.__collection
+    def loadOCRCollection(self):
+        return self.__ocrCollection
+
+    def loadUsersCollection(self):
+        return self.__usersCollection
 
     def insertOCRResult(self, resultDict):
         try:
-            self.__collection.insert_one(resultDict)
+            self.__ocrCollection.insert_one(resultDict)
         except Exception as e:
-            f"Error | {e}"
+            return f"Error insert results | {e}"
 
     def get(self):
         try:
-            results = self.__collection.find({})
+            results = self.__ocrCollection.find({})
         except Exception as e:
-            return f"Error | {e}"
+            return f"Error get documents | {e}"
         return results
+
+    def getUsers(self):
+        try:
+            results = self.__usersCollection.find({})
+        except Exception as e:
+            return f"Error get users | {e}"
+        return results
+    
+    def insertUser(self, userDict):
+        try:
+            self.__usersCollection.insert_one(userDict)
+        except Exception as e:
+            return f"Error insert user | {e}"
